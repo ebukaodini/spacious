d14f7db2e933ce1b5b09b7e94cf61a4e70366503
Ebuka Odini <ebukaodini@gmail.com>
2022-03-27 09:11:35 +0100

    feat: added the database queries for the resources

---
+++ server/src/db/queries/characters.js
@@ -0,0 +1,56 @@
+const knex = require('../knex');
+const { getAllPlanets } = require('./planets');
+
+function getAllCharacters() {
+  return knex.raw(`
+    SELECT id, name, description, picture_url AS "pictureUrl",
+      (SELECT json_object_agg('name', name) FROM planets WHERE code = planet) AS planet
+      FROM characters;
+  `).then(result => {
+    return result.rows
+  })
+}
+
+function getSingleCharacter(id) {
+  return knex.raw(`
+    SELECT id, name, description, picture_url AS "pictureUrl",
+      (SELECT json_object_agg('name', name) FROM planets WHERE code = planet) AS planet,
+      (SELECT array_agg(name) FROM characters AS T2 WHERE T2."id" IN (1,2)) AS friends
+      FROM characters AS T1 WHERE id = ${id} LIMIT 1;
+  `).then(result => {
+    return result.rows[0]
+  })
+
+  // SELECT array_agg(friends) FROM characters AS T3 WHERE T3."id" = T2."id"
+}
+
+function getLastAddedCharacter() {
+  return knex.raw(`
+    SELECT id, name, description, picture_url AS "pictureUrl",
+      (SELECT json_object_agg('name', name) FROM planets WHERE code = planet) AS planet
+      FROM characters ORDER BY id DESC LIMIT 1;
+  `).then(result => {
+    return result.rows[0]
+  })
+}
+
+async function addCharacter({ name, description, planet, pictureUrl, friends }) {
+  return knex.raw(`
+    INSERT INTO characters (name, description, planet, picture_url, friends)
+    VALUES (E'${name}', E'${description}', '${planet}', '${pictureUrl}', ?)
+  `, [friends]).then(result => {
+    if (result.rowCount === 1)
+      return getLastAddedCharacter()
+    else return false
+  })
+}
+
+module.exports = {
+  getAllCharacters,
+  getSingleCharacter,
+  addCharacter
+};
+
+
+// (SELECT COUNT(*) FROM characters WHERE planet = code) AS population,
+// (SELECT json_agg(name) FROM character_friends WHERE planet = code LIMIT 3) as characters
--- server/src/db/queries/planets.js
+++ server/src/db/queries/planets.js
@@ -1,1 +1,59 @@
 const knex = require('../knex');
+
+function getAllPlanets() {
+  return knex.raw(`
+    SELECT id, name, description, code, picture_url AS "pictureUrl",
+      (SELECT COUNT(*) FROM characters WHERE planet = code) AS population,
+      (SELECT json_agg(id) FROM characters WHERE planet = code LIMIT 3) as characters
+      FROM planets;
+  `).then(result => {
+    return result.rows
+  })
+}
+
+function getLastAddedPlanet() {
+  return knex.raw(`
+    SELECT id, name, description, code, picture_url AS "pictureUrl",
+      (SELECT COUNT(*) FROM characters WHERE planet = code) AS population,
+      (SELECT json_agg(id) FROM characters WHERE planet = code LIMIT 3) as characters
+      FROM planets ORDER BY id DESC LIMIT 1;
+  `).then(result => {
+    return result.rows[0]
+  })
+}
+
+function generatePlanetCode() {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const randomLetter = () => letters[Math.floor(Math.random() * 25) + 1];
+  const randomNumber = () => Math.floor(Math.random() * 9) + 1;
+
+  return `${randomLetter()}${randomLetter()}-${randomLetter()}${randomLetter()}${randomLetter()}-${randomNumber()}${randomNumber()}`
+}
+
+function addPlanet({ name, description, pictureUrl }) {
+  const code = generatePlanetCode()
+
+  while (
+    knex.raw(`
+    SELECT code FROM planets WHERE code = '${code}';
+  `).then(result => {
+      return result.rowCount == 0
+    }) === false
+  ) {
+    code = generatePlanetCode()
+  }
+
+  return knex.raw(`
+    INSERT INTO planets (name, description, code, picture_url)
+    VALUES (E'${name}', E'${description}', '${code}', '${pictureUrl}')
+  `).then(result => {
+    if (result.rowCount === 1)
+      return getLastAddedPlanet()
+    else return false
+  })
+}
+
+module.exports = {
+  getAllPlanets,
+  addPlanet
+};
