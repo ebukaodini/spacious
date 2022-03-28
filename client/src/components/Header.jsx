163209cc45edd5738cca7c412bba8e399e257165
Ebuka Odini <ebukaodini@gmail.com>
2022-03-29 00:03:26 +0100

    feat: added the header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,71 @@
+import { Link, useRouteMatch } from "react-router-dom"
+import styled from "styled-components"
+
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding-top: 32px;
+`
+const Title = styled.h1`
+  height: 64px;
+  width: 492px;
+  
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: 72px;
+  letter-spacing: 1px;
+  text-align: left;
+`
+const Menu = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px;
+
+  width: 279px;
+  height: 40px;
+
+  margin-top: 24px;
+`
+const Tab = styled(Link)`
+  text-decoration: none;
+  color: #121C33;
+  
+  border-radius: 8px;
+  border: none;
+  padding: 8px 24px;
+  
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px;
+  letter-spacing: 0px;
+  text-align: center;
+
+  cursor: pointer;
+  
+  :nth-child(2) {
+    margin: 0px 8px;
+  }
+
+  :hover,
+  &.active {
+    background: rgba(18, 28, 51, 0.05);
+  }
+`
+
+export const Header = () => {
+
+  const { path } = useRouteMatch()
+  
+  return (
+    <Wrapper>
+      <Title>Spacious</Title>
+      <Menu>
+        <Tab to='/' className={path === '/' && 'active'}>PLANETS</Tab>
+        <Tab to='/characters' className={path === '/characters' && 'active'}>CHARACTERS</Tab>
+      </Menu>
+    </Wrapper>
+  )
+}
