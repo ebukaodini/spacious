064eb40539d06f9041ab3158b30c26164c99164f
Ebuka Odini <ebukaodini@gmail.com>
2022-03-28 10:23:23 +0100

    refactor: updated the models for graphql

--- server/src/db/models/characters.js
+++ server/src/db/models/characters.js
@@ -1,24 +1,36 @@
 const knex = require('../knex');
 
-function getAllCharacters() {
+function getAllCharacters({ pageSize, page }) {
   return knex.raw(`
     SELECT id, name, description, picture_url AS "pictureUrl",
       (SELECT json_object_agg('name', name) FROM planets WHERE code = planet) AS planet
       FROM characters;
   `).then(result => {
-    return result.rows
+    return {
+      pagination: {
+        page, pageSize
+      },
+      nodes: result.rows
+    }
   })
+    .then(characters => {
+      return knex.raw(`SELECT COUNT(*) FROM characters`)
+        .then(res => {
+          characters.pagination.total = parseInt(res.rows[0].count)
+          return characters
+        })
+    })
 }
 
 function getSingleCharacter(id) {
   return knex.raw(`
     SELECT id, name, description, picture_url AS "pictureUrl",
-      (SELECT json_object_agg('name', name) FROM planets WHERE code = planet) AS planet,
-      (SELECT array_agg(name) FROM characters AS T2 WHERE T2."id" IN (1,2)) AS friends
-      FROM characters AS T1 WHERE id = ${id} LIMIT 1;
-  `).then(result => {
-    return result.rows[0]
-  })
+      (SELECT json_object_agg('name', name) FROM planets WHERE code = planet) AS planet
+      FROM characters WHERE id = ${id} LIMIT 1;
+  `)
+    .then(result => {
+      return result.rows[0]
+    })
 
   // SELECT array_agg(friends) FROM characters AS T3 WHERE T3."id" = T2."id"
 }
@@ -28,20 +40,22 @@ function getLastAddedCharacter() {
     SELECT id, name, description, picture_url AS "pictureUrl",
       (SELECT json_object_agg('name', name) FROM planets WHERE code = planet) AS planet
       FROM characters ORDER BY id DESC LIMIT 1;
-  `).then(result => {
-    return result.rows[0]
-  })
+  `)
+    .then(result => {
+      return result.rows[0]
+    })
 }
 
 async function addCharacter({ name, description, planet, pictureUrl, friends }) {
   return knex.raw(`
     INSERT INTO characters (name, description, planet, picture_url, friends)
     VALUES (E'${name}', E'${description}', '${planet}', '${pictureUrl}', ?)
-  `, [friends]).then(result => {
-    if (result.rowCount === 1)
-      return getLastAddedCharacter()
-    else return false
-  })
+  `, [friends])
+    .then(result => {
+      if (result.rowCount === 1)
+        return getLastAddedCharacter()
+      else return {}
+    })
 }
 
 module.exports = {
--- server/src/db/models/planets.js
+++ server/src/db/models/planets.js
@@ -1,25 +1,50 @@
 const knex = require('../knex');
 
-function getAllPlanets() {
+function getAllPlanets(pageSize, page) {
+
+  let offset = (pageSize * page) - pageSize
+
   return knex.raw(`
     SELECT id, name, description, code, picture_url AS "pictureUrl",
       (SELECT COUNT(*) FROM characters WHERE planet = code) AS population,
-      (SELECT json_agg(id) FROM characters WHERE planet = code LIMIT 3) as characters
-      FROM planets;
-  `).then(result => {
-    return result.rows
+      (SELECT array_agg(id) FROM characters WHERE planet = code LIMIT 3) as characters
+      FROM planets LIMIT ${pageSize} OFFSET ${offset};
+  `).then(async result => {
+    const planets = result.rows;
+
+    let index = 0;
+    planets.forEach(row => {
+      planets[index].characters = row.characters.map(character => ({ id: character }))
+      index++;
+    })
+
+    return {
+      pagination: {
+        page, pageSize
+      },
+      nodes: planets
+    }
   })
+    .then(planets => {
+      return knex.raw(`SELECT COUNT(*) FROM planets`)
+        .then(res => {
+          planets.pagination.total = parseInt(res.rows[0].count)
+
+          return planets
+        })
+    })
 }
 
 function getLastAddedPlanet() {
   return knex.raw(`
     SELECT id, name, description, code, picture_url AS "pictureUrl",
       (SELECT COUNT(*) FROM characters WHERE planet = code) AS population,
-      (SELECT json_agg(id) FROM characters WHERE planet = code LIMIT 3) as characters
+      (SELECT array_agg(id) FROM characters WHERE planet = code LIMIT 3) as characters
       FROM planets ORDER BY id DESC LIMIT 1;
-  `).then(result => {
-    return result.rows[0]
-  })
+  `)
+    .then(result => {
+      return result.rows[0]
+    })
 }
 
 function generatePlanetCode() {
@@ -43,14 +68,17 @@ function addPlanet({ name, description, pictureUrl }) {
     code = generatePlanetCode()
   }
 
+  description = description.replace("'", "\\'")
+
   return knex.raw(`
     INSERT INTO planets (name, description, code, picture_url)
     VALUES (E'${name}', E'${description}', '${code}', '${pictureUrl}')
-  `).then(result => {
-    if (result.rowCount === 1)
-      return getLastAddedPlanet()
-    else return false
-  })
+  `)
+    .then(async result => {
+      if (result.rowCount === 1)
+        return getLastAddedPlanet()
+      else return {}
+    })
 }
 
 module.exports = {
