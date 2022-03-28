8e3550008ee3c66da9c33a6c6fceb2ee234627d7
Ebuka Odini <ebukaodini@gmail.com>
2022-03-29 00:01:05 +0100

    refactor: renamed App.js to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import { PlanetsPage, CreatePlanetPage, CharactersPage, CreateCharacterPage } from './pages'
 
 function App() {
@@ -9,10 +9,18 @@ function App() {
         <Route exact path='/planets/create' component={CreatePlanetPage} />
         <Route exact path='/characters' component={CharactersPage} />
         <Route exact path='/character/create' component={CreateCharacterPage} />
-        <Route render={() => (<>Error 404<br />Sorry!</>)} />
+        <Route render={() => (
+          <center>
+            Page not found!<br />
+            Seems like you're lost in space.<br />
+            <Link to='/'>Follow this Guide.</Link>
+          </center>
+        )} />
       </Switch>
     </BrowserRouter>
   );
 }
+
+
 
 export default App;
