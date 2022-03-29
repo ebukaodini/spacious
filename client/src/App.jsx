daeeebab40e8c824d8b13e71815a9d3f53f2083f
Ebuka Odini <ebukaodini@gmail.com>
2022-03-29 22:05:12 +0100

    refactor: minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,24 @@
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
-import { PlanetsPage, CreatePlanetPage, CharactersPage, CreateCharacterPage } from './pages'
+import { Switch, Route, Link } from 'react-router-dom'
+import { PlanetsPage, CharactersPage } from './pages'
 
 function App() {
+
   return (
-    <BrowserRouter>
+    <>
       <Switch>
+
         <Route exact path='/' component={PlanetsPage} />
-        <Route exact path='/planets/create' component={CreatePlanetPage} />
         <Route exact path='/characters' component={CharactersPage} />
-        <Route exact path='/character/create' component={CreateCharacterPage} />
         <Route render={() => (
           <center>
             Page not found!<br />
             Seems like you're lost in space.<br />
-            <Link to='/'>Follow this Guide.</Link>
+            <Link to='/'>Follow this Portal.</Link>
           </center>
         )} />
+
       </Switch>
-    </BrowserRouter>
+    </>
   );
 }
 
