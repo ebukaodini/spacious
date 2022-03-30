50804e4eb49fc778702afdbcb3862bb89a39fc75
Ebuka Odini <ebukaodini@gmail.com>
2022-03-30 15:42:28 +0100

    refactor: updated the flyout menu

--- client/src/components/FlyoutMenu.jsx
+++ client/src/components/FlyoutMenu.jsx
@@ -10,10 +10,12 @@ const FlyoutMenuWrapper = styled.div`
 `
 const Menu = styled.div`
   border-radius: 32px;
-  height: calc(100% - 48px);
+  height: calc(100% - 96px);
 
   margin-top: 32px;
   margin-bottom: 16px;
+
+  padding: 24px;
   
   background: #FFFFFF;
   box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
