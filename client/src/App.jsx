01779de5e8b9054867c575795e592065e49de866
Ebuka Odini <ebukaodini@gmail.com>
2022-03-30 15:45:57 +0100

    feat: added the support for modal routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,51 @@
-import { Switch, Route, Link } from 'react-router-dom'
-import { PlanetsPage, CharactersPage } from './pages'
+import { Switch, Route, Link, useLocation } from 'react-router-dom'
+import { PlanetsPage, CharactersPage, CreatePlanetPage, CreateCharacterPage } from './pages'
 
 function App() {
 
+  // NOTE:
+  // This modal routing approach is not the possible best approach
+  // This is just an 'Ad hoc' approach
+
+  const { pathname, state } = useLocation()
+
+  let previousComponent;
+  switch (localStorage.getItem('lastPath')) {
+    case '/characters':
+      previousComponent = CharactersPage
+      break;
+
+    default:
+      previousComponent = PlanetsPage
+      break;
+  }
+
+  let modalComponent;
+  if (state && state.modal) {
+    switch (pathname) {
+      case '/planets/create':
+        modalComponent = CreatePlanetPage
+        break;
+      case '/characters/create':
+        modalComponent = CreateCharacterPage
+        break;
+      default:
+        break;
+    }
+  } else {
+    localStorage.setItem('lastPath', pathname)
+  }
+
   return (
     <>
+
       <Switch>
 
         <Route exact path='/' component={PlanetsPage} />
         <Route exact path='/characters' component={CharactersPage} />
+        <Route exact path='/planets/create' component={previousComponent} />
+        <Route exact path='/characters/create' component={previousComponent} />
+
         <Route render={() => (
           <center>
             Page not found!<br />
@@ -18,6 +55,12 @@ function App() {
         )} />
 
       </Switch>
+
+      {
+        (state && state.modal) && modalComponent()
+      }
+
+
     </>
   );
 }
