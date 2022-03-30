ec5b7dd1e2d2df2c82232cd28223b051198ef256
Ebuka Odini <ebukaodini@gmail.com>
2022-03-30 15:38:22 +0100

    refactor: refactored the seed

--- server/src/db/seeds/test/02_characters.js
+++ server/src/db/seeds/test/02_characters.js
@@ -7,7 +7,7 @@ exports.seed = async function (knex) {
     DELETE FROM characters;
 
     INSERT INTO characters (name, description, planet, picture_url) VALUES 
-    ('Chewbacca', 'Chewbacca, known affectionately to his friends as Chewie, is a Wookiee male warrior, smuggler, mechanic, pilot, and resistance fighter.', 'FN-BBA-22', 'https://upload.wikimedia.org/wikipedia/en/6/6d/Chewbacca-2-.jpg'),
+    ('Chewbacca', 'Chewbacca, known affectionately to his friends as Chewie, is a Wookiee male warrior, smuggler, mechanic, pilot, and resistance fighter.', 'FN-BBA-22', 'https://image.shutterstock.com/image-photo/cannes-france-may-15-chewbacca-600w-1123987313.jpg'),
     ('Norbert Ériu', 'Norbert is a farmer.', 'FN-BBA-22', 'https://images.unsplash.com/photo-1588422333078-44ad73367bcb'),
     ('Sümeyye Sitora', 'Sümeyye is a teacher.', 'FN-BBA-22', 'https://images.unsplash.com/photo-1606103955054-99913abd77c8'),
     ('Cori Blagovesta', 'Cori is known as the most teasing person in the galaxy.', 'XT-FOE-43', 'https://images.unsplash.com/photo-1530071100468-90954e4921d0'),
